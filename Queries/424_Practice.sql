1f4fe095d0a3b500228f3969b4d1ce8584198ae1
Mia Greenberg <greenberg.mia@gmail.com>
2021-04-25 00:01:07 -0700

    Updates

--- Queries/424_Practice.sql
+++ Queries/424_Practice.sql
@@ -16,12 +16,14 @@ CREATE TABLE emp_no (
      PRIMARY KEY (emp_no)
 );
 
+DROP TABLE dept_manager
+
 CREATE TABLE dept_manager (
 dept_no VARCHAR(4) NOT NULL,
     emp_no INT NOT NULL,
     from_date DATE NOT NULL,
     to_date DATE NOT NULL,
-FOREIGN KEY (emp_no) REFERENCES emp_no (emp_no),
+FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
 FOREIGN KEY (dept_no) REFERENCES departments (dept_no),
     PRIMARY KEY (emp_no, dept_no)
 );
@@ -31,7 +33,7 @@ CREATE TABLE salaries (
   salary INT NOT NULL,
   from_date DATE NOT NULL,
   to_date DATE NOT NULL,
-  FOREIGN KEY (emp_no) REFERENCES emp_no (emp_no),
+  FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
   PRIMARY KEY (emp_no)
 );
 
@@ -40,18 +42,41 @@ CREATE TABLE dept_emp (
   dept_no VARCHAR NOT NULL,
   from_date DATE NOT NULL,
   to_date DATE NOT NULL,
-FOREIGN KEY (emp_no) REFERENCES emp_no (emp_no),
+FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
 FOREIGN KEY (dept_no) REFERENCES departments (dept_no),
   PRIMARY KEY (emp_no, dept_no)
 );
+
+DROP TABLE salaries
 
 CREATE TABLE Title (
   emp_no INT NOT NULL,
   title VARCHAR NOT NULL,
   from_date DATE NOT NULL,
   to_date DATE NOT NULL,
-FOREIGN KEY (emp_no) REFERENCES emp_no (emp_no),
+FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
   PRIMARY KEY (emp_no, title, from_date)
 );
 
-SELECT * FROM departments;
+SELECT * FROM title;
+
+-- Retirement elibitility
+SELECT first_name, last_name
+FROM employees
+WHERE (birth_date BETWEEN '1952-01-01' AND '1955-12-31') 
+AND (hire_date BETWEEN '1985-01-01' AND '1988-12-31');
+
+-- Number of employees retiring
+SELECT COUNT(first_name)
+FROM employees
+WHERE (birth_date BETWEEN '1952-01-01' AND '1955-12-31') 
+AND (hire_date BETWEEN '1985-01-01' AND '1988-12-31');
+
+-- Save as a csv
+SELECT first_name, last_name
+INTO retirement_info
+FROM employees
+WHERE (birth_date BETWEEN '1952-01-01' AND '1955-12-31') 
+AND (hire_date BETWEEN '1985-01-01' AND '1988-12-31');
+
+SELECT * FROM retirement_info
